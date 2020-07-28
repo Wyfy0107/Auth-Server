e905e8b8ffef8c0722652d51708b1f7765fe5fa8
Duy Nguyen <nguyenduy010798@gmail.com>
2020-07-28 23:06:56 +0300

    ready to deploy

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const authRoute = require("./routes/auth");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const postRoute = require("./routes/post");
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
 app.use(express.json());
 app.use("/api/user", authRoute);
 app.use("/api/post", postRoute);
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 mongoose
   .connect(process.env.DB_CONNECT, {
