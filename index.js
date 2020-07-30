553e23f188c75f1b0b67a079101ced17b6c387a5
Duy Nguyen <nguyenduy010798@gmail.com>
2020-07-30 19:18:54 +0300

    removed cors origin

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const cors = require("cors");
 
 dotenv.config();
 
-app.use(
-  cors({
-    origin: "https://jwt-api.netlify.app/",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/api/user", authRoute);
 app.use("/api/post", postRoute);
